ce70ef9e90eb2568dc7884a16e19b0de6c19a343
Oery <48154940+Oery@users.noreply.github.com>
2023-12-30 11:17:11 +0100

    Add About Banner

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,8 +4,7 @@ import "../styles/about.sass";
 function AboutPage() {
     return (
         <BaseLayout>
-            <h1>About</h1>
-            <p>This is the about page</p>
+            <Banner imageUrl="about_banner.jpg" />
         </BaseLayout>
     );
 }
