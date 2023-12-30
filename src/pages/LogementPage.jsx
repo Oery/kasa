580d8b200da79f45e6661786c71c6ff968232581
Oery <48154940+Oery@users.noreply.github.com>
2023-12-30 11:26:10 +0100

    Add Logement Route Logic

--- src/pages/LogementPage.jsx
+++ src/pages/LogementPage.jsx
@@ -1,10 +1,43 @@
 import BaseLayout from "../layouts/BaseLayout";
+import { useParams } from "react-router-dom";
+import ErrorNotFoundPage from "./ErrorNotFoundPage";
+import logements from "../data/logements.json";
 import "../styles/logement.sass";
 
 function LogementPage() {
+    let { id } = useParams();
+
+    const logement = logements.find((logement) => logement.id === id);
+
+    if (!logement) {
+        return <ErrorNotFoundPage />;
+    }
+
+    const {
+        title,
+        pictures,
+        location,
+        tags,
+        host,
+        rating,
+        description,
+        equipments,
+    } = logement;
 
     return (
         <BaseLayout>
+            <div className="logement">
+                <div className="info">
+                    <div>
+                        <h1 className="title">{title}</h1>
+                        <h6 className="location">{location}</h6>
+                    </div>
+                    <aside>
+                    </aside>
+                </div>
+                <div className="more-info">
+                </div>
+            </div>
         </BaseLayout>
     );
 }
