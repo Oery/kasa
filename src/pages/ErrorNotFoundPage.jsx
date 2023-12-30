95655b782986188228e73b424aef63b256742ad6
Oery <48154940+Oery@users.noreply.github.com>
2023-12-30 11:08:53 +0100

    Add Error Page

--- src/pages/ErrorNotFoundPage.jsx
+++ src/pages/ErrorNotFoundPage.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+import "../styles/error.sass";
+import BaseLayout from "../layouts/BaseLayout";
+
 function ErrorNotFoundPage() {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Page not found</p>
-        </div>
+        <BaseLayout>
+            <div className="error-page">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+                <Link to="/">Retourner sur la page d&apos;accueil</Link>
+            </div>
+        </BaseLayout>
     );
 }
 
