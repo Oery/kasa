32e908ade2a8c5a337ea1c0b8b322edd9393f505
Oery <48154940+Oery@users.noreply.github.com>
2023-12-30 11:06:01 +0100

    Add Proptypes

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,6 +1,14 @@
 import Header from "../components/Header";
+import PropTypes from "prop-types";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+
+BaseLayout.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]).isRequired,
+};
 
 function BaseLayout(props) {
     const { pathname } = useLocation();
