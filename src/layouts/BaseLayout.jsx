ad0be8b9188f126fe179cfedc34571051dab903a
Oery <48154940+Oery@users.noreply.github.com>
2023-12-30 11:05:03 +0100

    Add Scroll Reset on route change

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,6 +1,15 @@
 import Header from "../components/Header";
 
 function BaseLayout(props) {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        document.documentElement.scrollTo({
+            top: 0,
+            behavior: "instant",
+        });
+    }, [pathname]);
+
     return (
         <>
             <Header />
