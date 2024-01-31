bdd4ee4a048374b3f3c610214ecee934cca6b48a
Oery <48154940+Oery@users.noreply.github.com>
2024-01-31 16:04:17 +0100

    Add about data from json

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import BaseLayout from "../layouts/BaseLayout";
 import Banner from "../components/Banner";
 import Collapse from "../components/Collapse";
 import "../styles/about.sass";
+import data from "../data/about.json";
 
 function AboutPage() {
     return (
@@ -9,41 +10,11 @@ function AboutPage() {
             <Banner imageUrl="about_banner.jpg" imageAlt="Paysage montagneux" />
 
             <div className="about">
-                <Collapse title="Fiabilité">
-                    <p>
-                        Les annonces postées sur Kasa garantissent une fiabilité
-                        totale. Les photos sont conformes aux logements, et
-                        toutes les informations sont régulièrement vérifiées par
-                        nos équipes.
-                    </p>
-                </Collapse>
-                <Collapse title="Respect">
-                    <p>
-                        La bienveillance fait partie des valeurs fondatrices de
-                        Kasa. Tout comportement discriminatoire ou de
-                        perturbation du voisinage entraînera une exclusion de
-                        notre plateforme.
-                    </p>
-                </Collapse>
-                <Collapse title="Service">
-                    <p>
-                        La bienveillance fait partie des valeurs fondatrices de
-                        Kasa. Tout comportement discriminatoire ou de
-                        perturbation du voisinage entraînera une exclusion de
-                        notre plateforme.
-                    </p>
-                </Collapse>
-                <Collapse title="Sécurité">
-                    <p>
-                        La sécurité est la priorité de Kasa. Aussi bien pour nos
-                        hôtes que pour les voyageurs, chaque logement correspond
-                        aux critères de sécurité établis par nos services. En
-                        laissant une note aussi bien à l&apos;hôte q&apos;au
-                        locataire, cela permet à nos équipes de vérifier que les
-                        standards sont bien respectés. Nous organisons également
-                        des ateliers sur la sécurité domestique pour nos hôtes.
-                    </p>
-                </Collapse>
+                {Object.keys(data).map((key) => (
+                    <Collapse title={key} key={key}>
+                        <p>{data[key]}</p>
+                    </Collapse>
+                ))}
             </div>
         </BaseLayout>
     );
